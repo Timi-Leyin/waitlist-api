bffab5567b55f53f3def82c923b6c64fb7694d05
Timileyin <timileyinoyelekan11@gmail.com>
2022-05-01 22:11:24 +0100

    Created error middleware

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import helmet from "helmet"
 import express from "express"
 import bodyParser from "body-parser"
 
+import error from "./middlewares/error"
 /*
  APPLICATION 
  @Express 
@@ -14,11 +15,12 @@ const app = express()
 dotenv.config()
 /* Log request only on development mode 👇 */
 process.env.NODE_ENV == "development" && app.use(morgan("dev"))
-app.use(cors())
+app.use(cors({}))
 app.use(helmet())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true }))
 
+app.use(error)
 
 
 export default app
