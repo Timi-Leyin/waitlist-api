29c078747a0356f17841628de3d5de2926393655
Timileyin <timileyinoyelekan11@gmail.com>
2022-05-01 21:59:53 +0100

    Configure Project

--- server.ts
+++ server.ts
@@ -5,5 +5,5 @@ const PORT = ( process.env.PORT || process.env.DEV_PORT || 5500 ) as number
 * LISTEN 👂 
 */
 app.listen(PORT, ()=>{
-  console.log(`Server is running 🏃‍♂️ on`, PORT )
+  console.log(`Server is running 🏃‍♂️ on ${process.env.NODE_ENV} mode at ${PORT} ` )
 })
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import * as dotenv from "dotenv"
 import cors from "cors"
+import morgan from "morgan"
 import helmet from "helmet"
 import express from "express"
 import bodyParser from "body-parser"
@@ -11,6 +12,8 @@ import bodyParser from "body-parser"
 const app = express()
 
 dotenv.config()
+/* Log request only on development mode 👇 */
+process.env.NODE_ENV == "development" && app.use(morgan("dev"))
 app.use(cors())
 app.use(helmet())
 app.use(bodyParser.json())
